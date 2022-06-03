0c8417f09bc6492e3e17fa26954aa5cdd7ad3764
Bruno Minelli <minellibruno@hotmail.com>
2022-06-03 19:04:48 -0300

    Add feat: Seleção de álbuns e seguidores de um artista.

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT
+	ar.artist AS artista,
+    al.album as album,
+	COUNT(*) AS seguidores
+FROM SpotifyClone.Following AS f
+INNER JOIN SpotifyClone.Users AS u
+	ON u.id = f.user_id
+INNER JOIN SpotifyClone.Artists AS ar
+	ON ar.id = f.artist_id
+INNER JOIN SpotifyClone.Albums as al
+	ON al.artist_id = ar.id
+GROUP BY ar.artist, al.album
+ORDER BY seguidores DESC, artista ASC, album ASC;
